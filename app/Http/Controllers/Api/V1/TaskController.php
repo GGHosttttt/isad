88ed3b97b301b2bae6362fd40f7a2b4e3a362ff6
GGHosttttt <Shiki.nokirin@gmail.com>
2024-12-14 13:40:29 +0700

    create task ban

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -6,12 +6,30 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 
 class TaskController extends Controller
 {
-    public function index(){
-        return Task::all();
+    public function create()
+    {
+
+    }
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+
+        return TaskResource::make($task);
+
+    }
+    public function show(Task $task)
+    {
+        return TaskResource::make($task);
+    }
+
+    public function index()
+    {
+        return TaskResource::collection(Task::all());
     }
 
 }
